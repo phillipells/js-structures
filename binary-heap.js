e03b43846e00fe2cf32911b85d7445bde3a19a7d
phillipells <phillip.ells1025@gmail.com>
2017-08-05 12:49:04 -0500

    adding bubble up function

--- binary-heap.js
+++ binary-heap.js
@@ -4,7 +4,7 @@ class BinaryHeap {
         this.size = data.length;
     }
 
-    heapify = () => {
+    heapify() {
         if(this.data && (this.data instanceof Array)) {
             for(let i = Math.floor((this.size - 1) / 2); i >= 0; i--) {
 
@@ -12,15 +12,38 @@ class BinaryHeap {
         }
     }
 
-    getLeftChild = (parentIndex) => {
+    getLeftChildIndex(parentIndex) {
         return 2 * parentIndex + 1;
     }
 
-    getRightChild = (parentIndex) => {
+    getRightChildIndex(parentIndex) {
         return 2 * parentIndex + 2;
     }
 
-    getParent = (childIndex) => {
+    getParentIndex(childIndex) {
         return Math.floor((childIndex - 1) / 2);
+    }
+
+    bubbleUp(childIndex, childData) {
+        if(childIndex > 0) {
+            
+            const parentIndex = this.getParentIndex(childIndex);
+            const parentData = this.data[parentIndex];
+
+            if(childData < parentData) {
+                this.data[parentIndex] = childData;
+                this.data[childIndex] = parentData;
+
+                this.bubbleUp(parentIndex, childData);
+            }
+        }
+        
+    }
+
+    insert(newItem) {
+        if(newItem) {
+            this.data.push(newItem);
+            this.bubbleUp(this.data.length - 1, newItem);
+        }
     }
 }
